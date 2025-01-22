d1c8201f8ffde50d0a29183c99a7230c71f4ef90
beep1000101 <matiponiatowski@o2.pl>
2025-01-22 17:42:27 +0100

    Add docstring to scrape_books function for improved documentation

--- scraping/pages.py
+++ scraping/pages.py
@@ -4,6 +4,21 @@ from bs4 import BeautifulSoup
 from utils.enums import SOUP, BOOKS
 
 def scrape_books(base_url, page_url):
+    """
+    Scrapes book URLs from a given page URL.
+
+    Parameters
+    ----------
+    base_url : str
+        The base URL of the website.
+    page_url : str
+        The URL of the page to scrape.
+
+    Returns
+    -------
+    list
+        A list of full URLs to the books found on the page.
+    """
     html = requests.get(page_url).text
     soup_books = BeautifulSoup(html, SOUP.HTML_PARSER)
     books = soup_books.find_all(SOUP.ANCHOR, class_=BOOKS.BOOK_CLASS)
@@ -11,4 +26,3 @@ def scrape_books(base_url, page_url):
     books_hrefs = [base_url + suffix for suffix in books_hrefs_suffixes]
 
     return books_hrefs
-    
