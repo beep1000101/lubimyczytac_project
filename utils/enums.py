9fb5a30c42841f8ebac868eeb812cc46280a1d8e
beep1000101 <matiponiatowski@o2.pl>
2025-01-22 17:30:31 +0100

    Add docstrings for URLS, SOUP, and BOOKS enumerations in enums module

--- utils/enums.py
+++ utils/enums.py
@@ -1,13 +1,21 @@
 from enum import StrEnum
 
 class URLS(StrEnum):
+    """
+    Enum for URL constants.
+    """
     BASE = 'base_url'
     PAGE = 'page_url'
 
 class SOUP(StrEnum):
+    """
+    Enum for BeautifulSoup parsing constants.
+    """
     HTML_PARSER = 'html.parser'
     ANCHOR = 'a'
 
 class BOOKS(StrEnum):
+    """
+    Enum for book-related CSS classes.
+    """
     BOOK_CLASS = 'authorAllBooks__singleTextTitle float-left'
-    
