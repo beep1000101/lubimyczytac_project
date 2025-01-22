20c63d665f1fc0d391a282d8b8b95d1fbdf0c8db
beep1000101 <matiponiatowski@o2.pl>
2025-01-22 17:34:04 +0100

    Fix syntax error in scrape_books function for finding book elements

--- scraping/pages.py
+++ scraping/pages.py
@@ -6,7 +6,7 @@ from utils.enums import SOUP, BOOKS
 def scrape_books(url):
     html = requests.get(url).text
     soup_books = BeautifulSoup(html, SOUP.HTML_PARSER)
-    books = soup_books.find_all(SOUP.ANCHOR), class_=BOOKS.BOOK_CLASS)
+    books = soup_books.find_all(SOUP.ANCHOR, class_=BOOKS.BOOK_CLASS)
     books_hrefs = [book['href'] for book in books]
 
     return books_hrefs
