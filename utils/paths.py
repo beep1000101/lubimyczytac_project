7a4439740f1345b0d2276cb8f6308b25e672da35
beep1000101 <matiponiatowski@o2.pl>
2025-01-22 23:27:35 +0100

    Add data paths for books and data directory in paths utility

--- utils/paths.py
+++ utils/paths.py
@@ -3,3 +3,5 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent.parent
 CONFIG_PATH = ROOT_PATH / 'configs'
 URL_CONFIG_PATH = CONFIG_PATH / 'url_config.json'
+DATA_PATH = ROOT_PATH / 'data'
+BOOKS_PATH = DATA_PATH / 'books'
