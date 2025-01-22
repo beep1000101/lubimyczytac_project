3dc74a3a5c6374006119aa5cbf268539066bb7fd
beep1000101 <matiponiatowski@o2.pl>
2025-01-22 23:34:51 +0100

    Update BOOKS_PATH to point to books_data.csv in paths utility

--- utils/paths.py
+++ utils/paths.py
@@ -4,4 +4,4 @@ ROOT_PATH = Path(__file__).parent.parent
 CONFIG_PATH = ROOT_PATH / 'configs'
 URL_CONFIG_PATH = CONFIG_PATH / 'url_config.json'
 DATA_PATH = ROOT_PATH / 'data'
-BOOKS_PATH = DATA_PATH / 'books'
+BOOKS_PATH = DATA_PATH / 'books_data.csv'
