6b7d74642ae2905da25a1084dba6788949cbd21a
beep1000101 <matiponiatowski@o2.pl>
2025-01-22 17:40:49 +0100

    Refactor scrape_books function to accept base_url and page_url parameters, and update href construction for book links

--- scraping/pages.py
+++ scraping/pages.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 
 from utils.enums import SOUP, BOOKS
 
-def scrape_books(url):
-    html = requests.get(url).text
+def scrape_books(base_url, page_url):
+    html = requests.get(page_url).text
     soup_books = BeautifulSoup(html, SOUP.HTML_PARSER)
     books = soup_books.find_all(SOUP.ANCHOR, class_=BOOKS.BOOK_CLASS)
-    books_hrefs = [book['href'] for book in books]
+    books_hrefs_suffixes = [book['href'] for book in books]
+    books_hrefs = [base_url + suffix for suffix in books_hrefs_suffixes]
 
     return books_hrefs
     
